8adab2384875121addc1df1522b7d1ec36267091
juicyjung <travelandi01@gmail.com>
2023-06-07 02:09:00 +0900

    string represents

--- torch_demo.py
+++ torch_demo.py
@@ -5,6 +5,9 @@ from PIL import Image
 from torchvision import transforms
 
 
+
+number = 12
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
@@ -23,7 +26,7 @@ transform = transforms.Compose([
 ])
 
 # Load and preprocess the image
-img_path = 'e_data/5.png'
+img_path = f'e_data/{number}.png'
 image = Image.open(img_path).convert('RGB')
 image = transform(image).unsqueeze(0).to(device)  # add batch dimension and send image to the device
 
@@ -36,22 +39,29 @@ x_pred, y_pred = output[0]
 x_pred = x_pred.item()
 y_pred = y_pred.item()
 
-print(x_pred, y_pred)
+print("\n\nprediction : ", x_pred, y_pred)
 
 
 
+import pandas as pd
+
+file = pd.read_csv('new_xy_data.csv')
+print("label : ", file.iloc[number-1]['x'], file.iloc[number-1]['y'])
+
+
+if x_pred > 0: x_pred_rep = 'left'
+else : x_pred_rep = 'right'
+
+if y_pred > 0: y_pred_rep = 'downside'
+else : y_pred_rep = 'upside'
+
+print(f'\n{abs(x_pred) : .3f} to the {x_pred_rep}, {abs(y_pred) : .3f} to the {y_pred_rep}')
 
 
 
-# Import SixDRepNet
-from sixdrepnet import SixDRepNet
 import cv2
 
-img = cv2.imread('e_data/5.png')
+img = cv2.imread(f'e_data/{number}.png')
 
-model = SixDRepNet()
-model.draw_axis(img, 0, 25.558958574313053, 31.55232731345202)     # -25.558958574313053,-31.55232731345202  # 0, -y_pred, x_pred
-
-
-cv2.imshow("test_window", img)
+cv2.imshow("The larger X the more left, The larger Y more down", img)
 cv2.waitKey(0)
